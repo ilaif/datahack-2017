4ce1c043bf2d7f30ff2b128ff5bb43a47fdd5f46
Ilai Fallach <ifallach@gmail.com>
2017-10-26 01:47:12 +0300

    [CHANGE] modules working.

--- src/main.py
+++ src/main.py
@@ -8,11 +8,12 @@ files = ['Economics.txt', 'History_US.txt', 'Psychology.txt', 'Government.txt', 
 def load_questions():
     questions = []
     for f_name in files:
+
         category = f_name.replace('.txt', '')
         path = folder + f_name
-        c = []
         with open(path) as f:
             c = f.read().replace('\r\n\r\n', '\n\n').replace('\r\n', '\n').split('\n\n')
+
         question_raw = [q.split('\n') for q in c]
         for q in question_raw:
             q = [l for l in q if l != '']
@@ -20,9 +21,7 @@ def load_questions():
             if len(q) < 1:
                 # Print here to see what we dismissed
                 continue
-            question = Question()
-            question.set_question(q[0])
-            question.set_category(category)
+            question = Question(question=q[0], category=category)
             for a in q[1:]:
                 is_correct, answer = a.split(' ', 1)
                 is_correct = (is_correct == '1')
